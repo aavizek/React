416fddff935925eb24c624fd595314598a4ef386
Abhishek Sharma <abhi.sharma0299@gmail.com>
2019-05-05 01:04:09 +0530

    Updates

---
+++ precache-manifest.6339f03763a5c8fc0a944fe619b43580.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0019fd6559fd34ad0edd99bfc7ba63db",
+    "url": "/React/index.html"
+  },
+  {
+    "revision": "8681f27503f974357b6a",
+    "url": "/React/static/css/main.d300bf4a.chunk.css"
+  },
+  {
+    "revision": "37158eba79900f233b92",
+    "url": "/React/static/js/2.05fac69a.chunk.js"
+  },
+  {
+    "revision": "8681f27503f974357b6a",
+    "url": "/React/static/js/main.8d7c1e07.chunk.js"
+  },
+  {
+    "revision": "283b01c4b2c734b504b3",
+    "url": "/React/static/js/runtime~main.d53f88d2.js"
+  }
+]);
